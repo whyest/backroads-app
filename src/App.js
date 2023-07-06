08e5232f5bea31ba297c864833e5a078a45b03d7
whyest <whyest@gmail.com>
2023-07-06 17:32:02 +0300

    added readme, updated data, added images, added components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Services from './components/Services'
+import About from './components/About'
+import Tours from './components/Tours'
+import Footer from './components/Footer'
+
 function App() {
   return (
-    <div className='App'>
-      <h1>Backroads app</h1>
+    <div>
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <Tours />
+      <Footer />
     </div>
   )
 }
---
+++ src/data.js
@@ -0,0 +1,13 @@
+export const pageLinks = [
+  { id: 1, href: '#home', text: 'home' },
+  { id: 2, href: '#about', text: 'about' },
+  { id: 3, href: '#services', text: 'services' },
+  { id: 4, href: '#tours', text: 'tours' },
+]
+
+export const socialLinks = [
+  { id: 1, href: 'https://www.twitter.com', icon: 'fab fa-twitter' },
+  { id: 2, href: 'https://www.facebook.com', icon: 'fab fa-facebook' },
+  { id: 3, href: 'https://www.telegram.com', icon: 'fab fa-telegram' },
+  { id: 4, href: 'https://www.instagram.com', icon: 'fab fa-instagram' },
+]
